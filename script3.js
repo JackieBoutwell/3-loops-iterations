baf3f86d247a5e7fa61ad10433daf3053fd961bf
Jackie Boutwell <boutwelljackie983@gmail.com>
2024-03-18 14:10:20 -0600

    went over labels and prime number

--- script3.js
+++ script3.js
@@ -212,7 +212,7 @@ while (change > 0) {
 let a = 10;
 
 while (a <= 90) {
-	console.log(a);
+	// console.log(a);
 	a += 20;
 }
 	
@@ -224,7 +224,7 @@ while (a <= 90) {
 let b = 20;
 
 while (b <= 100) {
-	console.log(b);
+	// console.log(b);
 	b += 20;
 }
 //can do this, this is long version i = i + 20;
@@ -239,7 +239,97 @@ while (b <= 100) {
 let c = 500;
 while (c <= 800) {
   if (c % 4 === 0) {
-    console.log(c);
+    // console.log(c);
   }
   c++;
 }
+
+for (let i = 0; i < 5; i++) {
+	console.log('outer: ', i);
+	for (let j = 0; j < 5; j++) {
+		console.log('inner:' ,j)
+	}
+}
+
+// Prime number (a number only divisible by itself and 1)
+// ex : 2, 3, 5, 7, 11, 13, 17, 19, 23, 29
+// 4 is not a prime number.
+
+for (let i = 2; i < 20; i++) {
+	
+	for (let j = 2; i < 20; i++) {
+
+		let isPrime = true;
+
+		for (let j = 2; j < i; j++) {
+			if (i % j === 0) {
+				// not prime
+				isPrime = false;
+				break;
+			}
+		}
+		// is(isPrime) {
+		//  console.log ( i, 'is prime')
+		// }
+	}
+
+}
+
+// Do while loop:
+let x = 10;
+
+do {
+	x--;
+	console.log(x);
+} while (x > 50);
+
+// labels example:
+
+myLoop: while (condition) {
+	// do the things
+}
+
+// Doing this allows us to break the outer loop from within the inner
+// loop, which would otherwise not be possible.
+// we can continue labeled loops from within nested loops.
+
+let x = 1;
+let y = 1;
+
+myLoop: while (true) {
+	console.log(`Outer loop ${x}.`);
+	x++;
+
+	while (true) {
+		console.log(`Inner loop ${y}.`);
+		y++;
+
+		if (x == 5 && y % 5 == 0) {
+			break myLoop;
+		} else if (y % 5 == 0) {
+			continue myLoop;
+		}
+	}
+}
+
+// for (let = i; i <5; i++)
+//break; - will break out of the loop
+//continue; with
+
+// while loops are good for if you don't know how many loops
+// you are doing
+
+// complicated loops would be a while loop.
+// coding problem or algorithms.
+// more flexible more control.
+
+
+let sum = 0;
+for (let i = 0; i < 5; i++) {
+	if (i % 2 === 0) continue;
+
+sum += i;
+
+}
+
+console.log(sum);
